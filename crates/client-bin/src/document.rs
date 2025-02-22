7ed4c74902f31815c81f67b097258e3f1b7b4e1c
Joachim Flottorp <joachim@flottorp.org>
2025-02-22 13:53:25 +0100

    Multiple documents

--- crates/client-bin/src/document.rs
+++ crates/client-bin/src/document.rs
@@ -1,16 +1,19 @@
-use std::io::{Read, Write};
-use std::sync::Arc;
-use iced::{Task, widget::{column, text}, Theme, Background, Color, Border, Shadow};
+use crate::network::tls_client::TlsClient;
 use iced::futures::AsyncReadExt;
-use iced::widget::{button, rich_text, span};
 use iced::widget::button::{Status, Style};
-use rustls::ClientConfig;
-use url::Url;
+use iced::widget::{button, rich_text, scrollable, span};
+use iced::{
+    widget::{column, text}, Background, Border, Color, Shadow, Task,
+    Theme,
+};
 use protocol::gemini_protocol::parse_response;
 use protocol::gemini_protocol::response::{OkResponse, Response};
 use protocol::gemtext::gemtext_body::Line;
 use protocol::gemtext::parse_gemtext;
-use crate::network::tls_client::TlsClient;
+use rustls::ClientConfig;
+use std::io::{Read, Write};
+use std::sync::Arc;
+use url::Url;
 
 #[derive(Debug, Clone)]
 pub enum LoadStatus {
@@ -28,61 +31,73 @@ pub enum DocumentMessage {
 #[derive(Debug, Clone)]
 pub struct DocumentData {
     url: Url,
-    content: OkResponse
+    content: OkResponse,
 }
 
 #[derive(Debug)]
 pub enum Document {
     Loading,
     Error(Url, Response),
-    Loaded(DocumentData)
+    Loaded(DocumentData),
 }
 
 impl Document {
     pub fn new(tls_client: Arc<ClientConfig>, url: Url) -> (Self, Task<DocumentMessage>) {
-        (Self::Loading, Task::perform(Self::load_document(tls_client, url.clone()), DocumentMessage::LoadComplete))
+        (
+            Self::Loading,
+            Task::perform(
+                Self::load_document(tls_client, url.clone()),
+                DocumentMessage::LoadComplete,
+            ),
+        )
+    }
+
+    pub fn title(&self) -> String {
+        match self {
+            Self::Loading => "Loading...".to_string(),
+            Self::Error(url, ..) => format!("Error {}", url),
+            Self::Loaded(data) => data.url.to_string(),
+        }
     }
 
     pub fn update(&mut self, message: DocumentMessage) -> Task<DocumentMessage> {
         match self {
             Self::Loading => {
                 match message {
-                    DocumentMessage::LoadComplete((url, Ok(data))) => {
-                        match data {
-                            LoadStatus::Success(data) => {
-                                *self = Self::Loaded(data);
-                            },
-                            LoadStatus::Error(response) => {
-                                *self = Self::Error(url, response);
-                            }
+                    DocumentMessage::LoadComplete((url, Ok(data))) => match data {
+                        LoadStatus::Success(data) => {
+                            *self = Self::Loaded(data);
+                        }
+                        LoadStatus::Error(response) => {
+                            *self = Self::Error(url, response);
                         }
                     },
                     DocumentMessage::LoadComplete((url, Err(error))) => {
                         eprintln!("Failed to load document: {}", error);
 
-                        *self = Self::Error(url, Response::PermanentFailure(Some(format!("Failed to load document: {}", error))));
-                    },
-                    _ => ()
+                        *self = Self::Error(
+                            url,
+                            Response::PermanentFailure(Some(format!(
+                                "Failed to load document: {}",
+                                error
+                            ))),
+                        );
+                    }
+                    _ => (),
                 };
 
                 Task::none()
-            },
-            Self::Error(_, _) => Task::none(),
-            Self::Loaded(doc) => {
-                match message {
-                    DocumentMessage::FIX_THIS => {
-                        Task::none()
-                    },
-                    DocumentMessage::LinkPressed(url) => {
-                        log::info!("Link pressed: {}", url);
-
-                        Task::none()
-                    }
-                    _ => {
-                        Task::none()
-                    }
-                }
             }
+            Self::Error(_, _) => Task::none(),
+            Self::Loaded(doc) => match message {
+                DocumentMessage::FIX_THIS => Task::none(),
+                DocumentMessage::LinkPressed(url) => {
+                    log::info!("Link pressed: {}", url);
+
+                    Task::none()
+                }
+                _ => Task::none(),
+            },
         }
     }
 
@@ -90,10 +105,8 @@ impl Document {
         match self {
             Self::Loading => text("Loading...").into(),
             Self::Error(url, response) => text(format!("Error: {}: {:?}", url, response)).into(),
-            Self::Loaded(data) =>  {
-                let mut columns = column![
-                    text(data.url.to_string()),
-                ];
+            Self::Loaded(data) => {
+                let mut columns = column![text(data.url.to_string())];
 
                 for line in &data.content.body.0 {
                     columns = match line {
@@ -109,14 +122,12 @@ impl Document {
                         }
 
                         Line::Heading { text: t, depth } => {
-                            let head = rich_text!(span(t))
-                                .size(10.0 + (10.0 * *depth as f32));
+                            let head = rich_text!(span(t)).size(10.0 + (10.0 * *depth as f32));
 
                             columns.push(head)
                         }
                         Line::ListItem(value) => {
-                            let item = rich_text!(span(value))
-                                .size(10.0);
+                            let item = rich_text!(span(value)).size(10.0);
 
                             columns.push(item)
                         }
@@ -135,21 +146,20 @@ impl Document {
         let r = match url.scheme() {
             "gemini" => Self::load_gemini(tls, &url).await,
             "file" => Self::load_file(&url).await,
-            _ => Err(format!("Unsupported scheme: {}", url.scheme()))
+            _ => Err(format!("Unsupported scheme: {}", url.scheme())),
         };
 
         (url, r)
     }
 
     async fn load_gemini(tls: Arc<ClientConfig>, url: &Url) -> Result<LoadStatus, String> {
-
         const DEFAULT_PORT: u16 = 1965;
 
         let host = url.host_str().ok_or("No host found")?;
         let port = url.port().unwrap_or(DEFAULT_PORT);
 
-        let mut conn = TlsClient::new_from_host((host, port), tls).
-            map_err(|e| format!("Failed to connect: {}", e))?;
+        let mut conn = TlsClient::new_from_host((host, port), tls)
+            .map_err(|e| format!("Failed to connect: {}", e))?;
 
         write!(conn, "{}\r\n", url.to_string()).unwrap();
 
@@ -174,22 +184,26 @@ impl Document {
         use async_std::fs::File;
 
         let path = url.path().strip_prefix("/").unwrap();
-        dbg!(&path);
-        let mut file = File::open(path).await.map_err(|e| format!("Failed to open file: {}", e))?;
+
+        let mut file = File::open(path)
+            .await
+            .map_err(|e| format!("Failed to open file: {}", e))?;
 
         let mut content = String::new();
-        file.read_to_string(&mut content).await.map_err(|e| format!("Failed to read file: {}", e))?;
+        file.read_to_string(&mut content)
+            .await
+            .map_err(|e| format!("Failed to read file: {}", e))?;
         let r = match parse_gemtext(&url, content) {
             Ok(r) => r,
-            Err(e) => return Err(format!("Failed to parse gemtext: {}", e))
+            Err(e) => return Err(format!("Failed to parse gemtext: {}", e)),
         };
 
         Ok(LoadStatus::Success(DocumentData {
             url: url.clone(),
             content: OkResponse {
                 mime: Default::default(),
-                body: r
-            }
+                body: r,
+            },
         }))
     }
 }
@@ -216,7 +230,7 @@ fn link_style(theme: &Theme, status: Status) -> Style {
             style.text_color = text;
 
             style
-        },
+        }
         Status::Hovered => {
             let mut text = text.clone();
             text.r = text.r * 1.1;
@@ -228,8 +242,7 @@ fn link_style(theme: &Theme, status: Status) -> Style {
             style.text_color = text;
 
             style
-        },
-        _ => style
+        }
+        _ => style,
     }
 }
-
--- crates/client-bin/src/main.rs
+++ crates/client-bin/src/main.rs
@@ -1,56 +1,73 @@
-mod network;
 mod document;
+mod network;
 
-use std::sync::Arc;
-use iced::{Center, Task};
-use iced::widget::{button, column, row, scrollable, text_input, Row};
-use log::info;
-use rustls::ClientConfig;
-use url::Url;
 use crate::document::{Document, DocumentMessage};
 use crate::network::tls_config::make_tls_config;
+use iced::widget::{button, column, row, scrollable, span, text, text_input, Button, Row, Text};
+use iced::{Center, Element, Length, Task};
+use log::info;
+use rustls::ClientConfig;
+use std::sync::Arc;
+use url::Url;
 
 #[derive(Debug, Clone)]
 pub enum GeminiRootMessage {
     Search,
     SearchBoxChanged(String),
-    DocumentMessage(usize, DocumentMessage)
+    DocumentMessage(usize, DocumentMessage),
+    DocumentHasLoaded(usize, DocumentMessage),
+    ViewDocument(usize),
 }
 
 #[derive(Debug)]
 pub struct GeminiRootWindow {
     tls_config: Arc<ClientConfig>,
     search_box: String,
-    documents: Vec<Document>
+    documents: Vec<Document>,
+    document_cursor: usize,
 }
 
 impl GeminiRootWindow {
     fn new() -> (Self, Task<GeminiRootMessage>) {
         // let url = Url::parse("gemini://geminiprotocol.net/").unwrap();
-        let url = Url::parse(&format!("file://{}/../../files/test.gemini", env!("CARGO_MANIFEST_DIR"))).unwrap();
+        let url = Url::parse(&format!(
+            "file://{}/../../files/test.gemini",
+            env!("CARGO_MANIFEST_DIR")
+        ))
+        .unwrap();
 
         let tls_config = make_tls_config().unwrap();
         let (document, task) = Document::new(tls_config.clone(), url);
 
         // let tasks = vec![task];
 
-        (Self {
-            tls_config,
-            search_box: String::new(),
-            // documents: Vec::new()
-            documents: vec![
-                document
-            ]
-            // }, Task::batch(tasks))
-        }, task.map(|d| GeminiRootMessage::DocumentMessage(0, d)))
+        (
+            Self {
+                tls_config,
+                search_box: String::new(),
+                documents: vec![document],
+                document_cursor: 0, // }, Task::batch(tasks))
+            },
+            task.map(|d| GeminiRootMessage::DocumentMessage(0, d)),
+        )
     }
 
     fn update(&mut self, message: GeminiRootMessage) -> Task<GeminiRootMessage> {
         match message {
             GeminiRootMessage::Search => {
-                info!("Searching");
+                info!("Search button pressed");
+                let url = if self.search_box.starts_with("gemini://") {
+                    Url::parse(&self.search_box).unwrap()
+                } else {
+                    Url::parse(&format!("gemini://{}", self.search_box)).unwrap()
+                };
+                let (document, task) = Document::new(self.tls_config.clone(), url);
+                self.documents.push(document);
 
-                Task::none()
+                let index = self.documents.len() - 1;
+                task.map(move |d| {
+                    return GeminiRootMessage::DocumentHasLoaded(index, d);
+                })
             }
             GeminiRootMessage::SearchBoxChanged(s) => {
                 info!("Search box changed to {}", s);
@@ -58,8 +75,7 @@ impl GeminiRootWindow {
 
                 Task::none()
             }
-            GeminiRootMessage::DocumentMessage(index, msg) =>
-            {
+            GeminiRootMessage::DocumentMessage(index, msg) => {
                 if let Some(document) = self.documents.get_mut(index) {
                     document
                         .update(msg)
@@ -68,19 +84,42 @@ impl GeminiRootWindow {
                     Task::none()
                 }
             }
+            GeminiRootMessage::DocumentHasLoaded(index, msg) => {
+                if let Some(document) = self.documents.get_mut(index) {
+                    self.document_cursor = index;
+
+                    document
+                        .update(msg)
+                        .map(move |msg| GeminiRootMessage::DocumentMessage(index, msg))
+                } else {
+                    Task::none()
+                }
+            }
+            GeminiRootMessage::ViewDocument(index) => {
+                self.document_cursor = index;
+                Task::none()
+            }
         }
     }
 
     fn view(&self) -> iced::Element<GeminiRootMessage> {
         let controls = self.view_controls();
 
-        // TODO: Multiple
+        let mut document_tabs = Row::new();
+        for (index, document) in self.documents.iter().enumerate() {
+            let url = document.title();
+            let b = Button::new(Text::new(url).width(Length::FillPortion(1)))
+                .on_press(GeminiRootMessage::ViewDocument(index));
+            let c = column![b];
+
+            document_tabs = document_tabs.push(c);
+        }
+
+        let document_tabs = scrollable(document_tabs.spacing(10));
+
         let document = self.view_document();
 
-        column![
-            controls,
-            document
-        ]
+        column![controls, document_tabs, document]
             .spacing(10)
             .padding(10)
             .into()
@@ -88,32 +127,23 @@ impl GeminiRootWindow {
 
     fn view_controls(&self) -> Row<GeminiRootMessage> {
         row![
-            button("Search")
-                .on_press(GeminiRootMessage::Search),
+            button("Search").on_press(GeminiRootMessage::Search),
             text_input("Enter a URL", &self.search_box)
                 .on_input(GeminiRootMessage::SearchBoxChanged)
                 .on_submit(GeminiRootMessage::Search)
         ]
-            .spacing(10)
-            .align_y(Center)
+        .spacing(10)
+        .align_y(Center)
     }
 
     fn view_document(&self) -> iced::Element<GeminiRootMessage> {
-        let document_views = self.documents
-            .iter()
-            .enumerate()
-            .map(|(index, document)| {
+        match self.documents.get(self.document_cursor) {
+            None => text!("THIS SHOULD NEVER HAPPEN ({})", self.document_cursor).into(),
+            Some(document) => {
                 let view = document.view();
-                view.map(move |msg| GeminiRootMessage::DocumentMessage(index, msg))
-            })
-            .collect::<Vec<_>>();
-
-        scrollable(
-            column!()
-                .padding(20)
-                .align_x(Center)
-                .extend(document_views)
-        ).into()
+                view.map(move |msg| GeminiRootMessage::DocumentMessage(self.document_cursor, msg))
+            }
+        }
     }
 }
 
@@ -125,7 +155,11 @@ fn main() {
         .unwrap();
 
     // let mut window_size = Vec2::new(800.0, 600.0);
-    iced::application("Gemini Browser", GeminiRootWindow::update, GeminiRootWindow::view)
-        .run_with(GeminiRootWindow::new)
-        .unwrap();
+    iced::application(
+        "Gemini Browser",
+        GeminiRootWindow::update,
+        GeminiRootWindow::view,
+    )
+    .run_with(GeminiRootWindow::new)
+    .unwrap();
 }
